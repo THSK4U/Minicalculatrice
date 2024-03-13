a5a545681118fb96455f2df42b353cc26e0ca5cc
TAHA SELLAK <sellaktaha1@gmail.com>
2024-03-13 15:50:51 +0000

    Terminé HTML et CSS et commencer JavaScript.

---
+++ javascript.js
@@ -0,0 +1,5 @@
+const bottun =document.querySelectorAll('.calcul .button');
+const larg_input = document.querySelector('.larg input');
+const small_input = document.querySelector('.small input');
+
+
