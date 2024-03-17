e3b92b54e28556c15fc0716bcd7f58466f738e3b
THSK4U <157622490+THSK4U@users.noreply.github.com>
2024-03-17 18:00:26 +0000

    nettoyage du css&html ajouter d'autres operation

--- javascript.js
+++ javascript.js
@@ -3,15 +3,26 @@ function afficher(val) {
 }
 
 function solve() {
-    let array = document.querySelector(".small p").textContent.split('+');
-    let sum = 0;
-    for (let i = 0; i < array.length; i++) {
-        sum += parseFloat(array[i]);
-    }
-        document.querySelector(".larg p").textContent = sum;
-        document.querySelector(".small p").textContent = sum;
+    let vall = document.querySelector(".small p").textContent;
+    let numbers = vall.match(/\d+(\.\d+)?/g).map(Number);
+    let operators = vall.match(/[\+\-\*\/]/g);
+
+    let result = numbers[0];
+    for (let i = 0; i < operators.length; i++) {
+        if (operators[i] === '+') {
+            result += numbers[i + 1];
+        } else if (operators[i] === '-') {
+            result -= numbers[i + 1];
+        } else if (operators[i] === '*') {
+            result *= numbers[i + 1];
+        } else if (operators[i] === '/') {
+            result /= numbers[i + 1];
+        }
     }
 
+    document.querySelector(".larg p").textContent = result;
+    document.querySelector(".small p").textContent = result;
+}
 
 function clr() {
     document.querySelector(".small p").textContent = "0";
@@ -23,7 +34,7 @@ function clrlast(){
     input = document.querySelector(".small p").textContent;
 if (input.length == 1){
     document.querySelector(".small p").textContent = "0";
-}else if (input.length > 0){
+}else if (input.length > 1){
         input = input.slice(0, -1);
         document.querySelector(".small p").textContent = input;
     }
